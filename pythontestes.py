9ac3220457c2f6e084ec5b153784b599cf488684
Gabriel Lisboa <devopup08@gmail.com>
2025-07-09 12:43:23 -0300

    Update pythontestes.py

--- pythontestes.py
+++ pythontestes.py
@@ -1,41 +1,11 @@
-#Imports
-import time
+# imports
 
-# Variaveis
-nome = "Programador Python"
-idade = "18"
-peso = "41"
+# variaveis
 
-print("A seguir mostraremos as seguintes informações sobre você, encontrada nas nossas variáveis DEFINIDAS:")
+nome = input("Digite seu nom:")
+idade = input("Digite a sua idade:")
+peso = input("Digite seu peso")
 
-print("Carregando dados...")
-for i in range(101):
-    print(f"\rCarregando: {i}%", end="", flush=True)
-    time.sleep(0.01)
-    
-print()
-time.sleep(0.3)
-print("Entrando na váriavel NOME")
-time.sleep(0.2)
-print("Pegando dados..")
-time.sleep(0.2)
-print("Enviando payload..")
-time.sleep(0.2)
-print("Seu nome:", nome)
-time.sleep(2)
-print("Entrando na váriavel IDADE")
-time.sleep(0.2)
-print("Pegando dados..")
-time.sleep(0.2)
-print("Enviando payload..")
-time.sleep(2.0)
-print("Sua idade:", idade)
-time.sleep(1)
-print("Entrando na váriavel PESO")
-time.sleep(0.2)
-print("Pegando dados..")
-time.sleep(0.2)
-print("Enviando payload..")
-time.sleep(2.0)
-print("Seu peso:", peso)
-
+print(nome)
+print(idade)
+print(peso)
