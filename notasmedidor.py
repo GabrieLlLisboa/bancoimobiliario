aceec94923b42f6477c23eb7aff244217ec08fdf
Gabriel Lisboa <devopup08@gmail.com>
2025-07-22 21:03:27 -0300

    Create notasmedidor.py

---
+++ notasmedidor.py
@@ -0,0 +1,12 @@
+nota = int(input("Qual a sua nota?"))
+media = 6
+
+
+
+print(nota)
+
+if nota >= media:
+    print("Você está aprovado!")
+
+elif nota <= media:
+    print("Você não passou na média.")
